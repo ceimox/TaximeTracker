2a6602faa79513ee83942dd67be41d759b0c80a6
ceimox <cesar_ro11@hotmail.com>
2013-09-14 23:50:19 -0500

    second commit

--- core/views.py
+++ core/views.py
@@ -1,15 +1,13 @@
 from django.http import HttpResponse,Http404
 import datetime
 from django.shortcuts import render, render_to_response
+from core.models import UserProfile, Project, Task, Timer
 
 def showindex(request):
     return render(request, 'show.html',{'img':'index.png'})
 
 def showhome(request):
     return render(request, 'show.html',{'img':'home.png'})
-
-def home(request):
-    return render(request, 'home.html')
 
 def showregistration(request):
     return render(request, 'show.html',{'img':'registration.png'})
@@ -21,4 +19,34 @@ def showprojects(request):
     return render(request, 'show.html',{'img':'projects.png'})
 
 def showallprojects(request):
-    return render(request, 'show.html',{'img':'allprojects.png'})        
+    return render(request, 'show.html',{'img':'allprojects.png'})      
+
+def home(request):
+    c=UserProfile.objects.all()[0]    
+    pr=Project.objects.all()
+    if request.method == 'POST':
+        print "ES UN POSSSSSSSSSSST"
+        alldata=request.POST
+        taskname = alldata.get("taskname")
+        taskdescription = alldata.get("taskdescription")
+        projectname= alldata.get("projectname")
+        p=Project.objects.get(name=projectname)
+        print p
+        t=Task(name=taskname,description=taskdescription,user=c,project=p)
+        t.save()
+    
+    return render(request, 'home.html',{'projects':pr})  
+
+def projects(request):
+    if request.method == 'GET':
+        alldata=request.GET
+        projectname = alldata.get("projectname")
+        projectcost = alldata.get("projectcost")
+        p=Project(name=projectname,price_per_hour=projectcost)
+        p.save()
+    return render(request, 'projects.html') 
+
+def yourtasks(request):
+    c=UserProfile.objects.all()[0]
+    tasks=c.task_set.all()
+    return render(request, 'yourtasks.html',{'tasks':tasks}) 
--- taxitimetracker/urls.py
+++ taxitimetracker/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from core.views import home, showhome, showindex, showregistration, showalltasks, showprojects, showallprojects
+from core.views import yourtasks, home, projects, showhome, showindex, showregistration, showalltasks, showprojects, showallprojects
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -9,6 +9,8 @@ urlpatterns = patterns('',
     url(r'^$',showindex),
     url(r'^showhome',showhome),
     url(r'^home',home),
+    url(r'^projects',projects),
+    url(r'^yourtasks',yourtasks),
     url(r'^showregistration',showregistration),
     url(r'^showalltasks',showalltasks),
     url(r'^showprojects',showprojects),
