da67817aa92df81f20a06926f3edf8366283505f
ceimox <cesar_ro11@hotmail.com>
2013-10-31 17:07:15 -0500

    new settings.py

--- taxitimetracker/settings.py
+++ taxitimetracker/settings.py
@@ -11,12 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'taximetracker',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'taximetrackerdb',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sq1lite3:
+        'USER': 'taximetracker',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
@@ -127,6 +127,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'core',
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
