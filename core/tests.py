24d829b64464fb1faae6b65c5a590a6651c13e28
ceimox <cesar_ro11@hotmail.com>
2013-12-20 13:34:43 -0500

    change in tests

--- core/tests.py
+++ core/tests.py
@@ -1,11 +1,45 @@
 from django.test import TestCase, RequestFactory
 from core.models import UserProfile, Project, Task, Timer
 from views import home, projects, yourtasks
+from core.models import start_task, stop_task, stop_fast_task, first_div
+from core.models import stop_second_div, second_div, start_second_div
+from core.models import search_existing_project
 import datetime
+
+class StartTaskTest(TestCase):
+
+    def test_iniciar_tarea_para_cambiarle_el_estado_y_asignarle_un_timer(self):
+        user = UserProfile(username="cesar", password="1234")
+        user.save()
+        t = Task(name="tarea1", user=user)
+        t.save()
+        start_task(t)
+        tasks_started = Task.objects.filter(started=True).count() 
+        self.assertEqual(tasks_started,1)
+        temporal_timer = t.current_timer
+        self.assertEqual(temporal_timer, Timer.objects.all()[0])
+        timers = Timer.objects.all().count()
+        self.assertEqual(timers,1)
+
+class StopTaskTest(TestCase):
+    def test_detener_tarea_para_cambiarle_el_estado_y_liberar_su_timer_temporal(self):
+        user = UserProfile(username="cesar", password="1234")
+        user.save()
+        t = Task(name="tarea1", user=user)
+        t.save()
+        start_task(t)
+        stop_task(t)
+        tasks_stopped = Task.objects.filter(started=False).count() 
+        self.assertEqual(tasks_stopped,1)
+        temporal_timer = t.current_timer
+        self.assertEqual(temporal_timer, None)
+        timers = Timer.objects.all().count()
+        self.assertEqual(timers,1)
+
 
 class ProjectTest(TestCase):
 
-    def test_al_hacer_post_se_crea_un_proyecreto(self):
+    def test_al_hacer_post_se_crea_un_proyecto(self):
         factory = RequestFactory()
         request = factory.post("/projects")
         request.POST["projectname"] = "project1"
@@ -32,20 +66,6 @@ class HomeTest(TestCase):
         self.assertEqual(tasks_number, 1)
 
 class YourtaskTest(TestCase):
-
-    def test_al_entrar_en_form1_y_iniciar_tarea(self):
-        user= UserProfile(username="cesar",password="1234",id=1)
-        user.save()
-        factory = RequestFactory()
-        request = factory.post("/yourtasks")
-        t=Task(name="tarea1",user=user)
-        t.save()
-        request.POST["form_selected"] = "form1"
-        request.POST["task_selected"] = t.id
-        request.POST["choisebuttom"] = "Start"
-        result = yourtasks(request)
-        tasks_started = Task.objects.filter(started=True).count() 
-        self.assertEqual(tasks_started,1)
 
     def test_al_entrar_en_form1_y_detener_tarea(self):
         user= UserProfile(username="cesar",password="1234",id=1)
@@ -234,7 +254,7 @@ class TaskTest(TestCase):
         result = t1.calculate_time()
         self.assertEqual(result,2)
 
-    def calcular_costo_para_obtener_el_valor_total_por_una_tarea_con_todos_sus_tiempos(self):
+    def test_calcular_costo_para_obtener_el_valor_total_por_una_tarea_con_todos_sus_tiempos(self):
         user= UserProfile(username="cesar",password="1234")
         user.save()
         project=Project(name="test_project",price_per_hour=4000)
