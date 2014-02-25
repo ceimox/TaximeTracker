68dcef73b48f4e5a8eff771a02d9486b0f129b36
ceimox <cesar_ro11@hotmail.com>
2014-02-25 00:20:07 -0600

    test without warnings

--- taxitimetracker/settings.py
+++ taxitimetracker/settings.py
@@ -46,7 +46,7 @@ USE_I18N = True
 USE_L10N = True
 
 # If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
+USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
@@ -169,3 +169,8 @@ LOGGING = {
         },
     }
 }
+
+import warnings
+warnings.filterwarnings(
+        'error', r"DateTimeField received a naive datetime",
+        RuntimeWarning, r'django\.db\.models\.fields')
